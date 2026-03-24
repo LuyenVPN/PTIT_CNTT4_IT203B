372543d6a31150723726bb3e3d1b66575bbcb6d1
Đặng Văn Luyện <133982516+LuyenVPN@users.noreply.github.com>
2026-03-24 23:30:34 +0700

    Add files via upload

---
+++ src/Session14/utils/DBConnection.java
@@ -0,0 +1,19 @@
+package Session14.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/flash_sale_db";
+    private static final String USER = "root";
+    private static final String PASS = "luyen123";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws Exception {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        }
+        return connection;
+    }
+}
